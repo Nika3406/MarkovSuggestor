375cb110dcc6ee0c3097751cce0a789fd5410779
David <dzw5600@psu.edu>
2025-10-26 14:29:10 -0400

    Initial Files

---
+++ main.py
@@ -0,0 +1,8 @@
+from markov import HiddenMarkovModel
+
+OBSERVABLE = None
+HIDDEN = None
+TRANSFORMATION = None
+EMISSION = None
+
+hmm = HiddenMarkovModel(OBSERVABLE, HIDDEN, TRANSFORMATION, EMISSION)
---
+++ markov.py
@@ -0,0 +1,22 @@
+class HiddenMarkovModel:
+    def __init__(self, observable, hidden, transformation, emission):
+        pass
+
+    def emit(self, state):
+        """Emits the obserable state into a hidden state
+
+        Args: 
+            state (string): The current state in the Hidden Markov Model
+        """
+        pass
+
+    def transform():
+        """Transforms the hidden state into a observation
+        
+        Returns:
+            observation (string): The output observation of the Hidden Markov Model
+        """
+        pass
+
+
+    
